eac177b679a3a90b766d28d218255f86a7e9f578
yoshikazu hotta <hotta-nagoya-6163@au.com>
2022-07-07 22:16:48 +0900

    Update resolvers.js

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,3 +1,14 @@
-const resolvers = {};
+const resolvers = {
+  Query: {
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome()
+    }
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId)
+    }
+  }
+};
 
 module.exports = resolvers;
