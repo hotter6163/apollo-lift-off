fc68a16daeaca699837a344a8423a397e06d2685
yoshikazu hotta <hotta-nagoya-6163@au.com>
2022-07-15 09:51:11 +0900

    Update resolvers.js

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -16,9 +16,18 @@ const resolvers = {
     }
   },
   Mutation: {
-    incrementTrackViews: (_, { id }, { dataSources }) => {
-      return dataSources.trackAPI.incrementTrackViews(id)
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      const track = await dataSources.trackAPI.incrementTrackViews(id)
+      return {
+        code: 200,
+        success: true,
+        message: `Successfully incremented number of views for track ${id}`,
+        track,
+      }
     }
+  },
+  IncrementTrackViewsResponse: {
+
   }
 };
 
